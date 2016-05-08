18ce186b57eb8602c24be9b6797b3c5c0fc18345
DanielAbreu <dani_abreu87@hotmail.com>
2016-05-08 20:06:11 +0100

    StockManager object added and xml deserialization completed

---
+++ Trabalho 1/Super/Super/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Super
+{
+    [Serializable()]
+    public class Item
+    {
+        [XmlElement()]
+        public string Name {get; set;}
+        [XmlElement()]
+        public int Qtd { get; set; }
+    }
+
+    [Serializable()]
+    [XmlRoot("Stock")]
+    public class Stock
+    {
+        [XmlElement("Item")]
+        public List<Item> Items { get; set; }
+    }
+}
---
+++ Trabalho 1/Super/Super/StockManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Super
+{
+    public class StockManager
+    {
+        private IEnumerable<Item> _Stock;
+        public IEnumerable<Item> Stock
+        {
+            get
+            {
+                if(_Stock == null)
+                {
+                    _Stock = LoadStock();
+                }
+                return _Stock;
+            }
+        }
+
+        private string stockPath
+        {
+            get
+            {
+                if (ConfigurationManager.AppSettings["StockPath"] == null)
+                {
+                    throw new ArgumentNullException("Key StockPath not present in App.config");
+                }
+                return ConfigurationManager.AppSettings["StockPath"];
+            }
+        }
+
+
+        private IEnumerable<Item> LoadStock()
+        {
+            Stock stock = null;
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Stock));
+
+            StreamReader reader = new StreamReader(stockPath);
+            stock = (Stock)serializer.Deserialize(reader);
+            reader.Close();
+
+            return stock.Items;
+        }
+
+
+    }
+}
